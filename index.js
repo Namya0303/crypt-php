593c9df9ca777790d7d71746ce9037e6d396af9d
Naman Chandok <namanchandok1@gmail.com>
2020-06-30 14:45:07 +0530

    dark mode default

--- index.js
+++ index.js
@@ -34,10 +34,10 @@
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'light');
     }
     else {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'dark');
     }    
 }
     
@@ -46,12 +46,12 @@ function switchTheme(e) {
 toggleSwitch.addEventListener('change', switchTheme, false);
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }    
 }
     
@@ -62,8 +62,7 @@ const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
+    if (currentTheme === 'light') {
         toggleSwitch.checked = true;
-        document.getElementById("skill-img").style.filter = "invert(100)";
     }
 }
