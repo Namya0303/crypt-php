bcaaff1ba196407625f96fa563644b9dae98499d
Namya0303 <35605470+Namya0303@users.noreply.github.com>
2020-07-16 00:13:18 +0530

    ye le

---
+++ crypt-release/rules.php
@@ -0,0 +1,137 @@
+<html>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+
+<head>
+
+    <title>Crypt@trix | Ordina@trix 20.0</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,400italic,500,100italic,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Dosis|Open+Sans" rel="stylesheet">
+    <link href='main.css' rel='stylesheet' type='text/css'>
+    <link rel="icon" href="images/ordin.png" type="image/png">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body onload="myFunction()">
+
+    <!---  LOADER   --->
+
+    <div class="load" id="load">
+        <center>
+            <div class="loader">
+                <div class="rect1"></div>
+                <div class="rect2"></div>
+                <div class="rect3"></div>
+                <div class="rect4"></div>
+                <div class="rect5"></div>
+            </div>
+        </center>
+    </div>
+
+
+    <div class="mainBod" id="mainBod">
+
+        <!---  NAVBAR   --->
+
+        <div class="navbar">
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <a href="index" class="logo-nav"><img src="images/ordin.png"></a>
+                    <a class="button" href="https://discord.gg/GCbVdag" target="_blank">Join the Discord</a>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+        </div>
+
+        <!--- DARK MODE SWITCH --->
+
+        <div class="theme-switch-wrapper">
+            <label class="theme-switch" for="checkbox">
+                <input type="checkbox" id="checkbox" />
+                <div class="slider round">
+                    <center>
+                        <img src="images/sun.png" class="sun">
+                        <img src="images/moon.png" class="moon">
+                    </center>
+                </div>
+            </label>
+        </div>
+
+        <!--- HOME --->
+
+        <div class="row home fade">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div class="writen fade">
+                    <h1>Rules and Regulations</h1>
+                    <h3>General Rules</h3>
+                    <ul>
+                        <li>This event is open for all students of a registered school.</li>
+                        <li>All participants must join the <a href="https://discord.gg/GCbVdag" target="_blank">discord server</a> for Crypt@trix</li>
+                        <li>This Crypt Hunt will be spanning over 72 hours, commencing on 16th July'20. All teams will have to try and solve as many stages as possible.</li>
+                        <li>On logging into their account, participants will land on the first question. They must solve the standing question to proceed to the next one.</li>
+                        <li>If two participants tie up on a level, the one who solved it first would be given a higher rank.</li>
+                    </ul>
+                    <h3>Registration</h3>
+                    <ul>
+                        <li>The event is open for as many participants to register, however <b>multiple accounts by a single person are not allowed.</b> If Caught having multiple accounts, all accounts belonging to that person will be banned</li>
+                        <li>Just as for the rest of the events , the students of tagore International School Vasant Vihar will also be taking part in the hunt but the participation will not be competitive and the final leaderboard wont be including the students from our school</li>
+                        <li><b>FOR TAGOREANS ONLY:</b> Even though you guys wont be on the final leaderboard, dont worry we will award you for your merit</li>
+                    </ul>
+                    <h3>Hints & Level Skip</h3>
+                    <ul>
+                        <li>In case the admins feel participants are facing difficulties on a particular level, hints will be released publically totally at the admins discretion</li>
+                        <li>Hints can also be bought from the <a href="shop">shop</a> for 100 points. Upon buying a hint card, participants are required to send a screenshot of the same to the admins to redeem their hints</li>
+                        <li><a href="shop">The shop</a> also has a level skip card for 350 points. Participants are however discouraged to use the same</li>
+                        <li>Admins with the tag 'Leaders' will be confirming leads on dm (a bad pun proundly intended) </li>
+                        <li>All important information and hints for this event will be released on the <a href="https://discord.gg/GCbVdag" target="_blank">discord server</a></li>
+                    </ul>
+                    <h3>Awards</h3>
+                    <ul>
+                        <li><b>Certificates will not be issues for participation</b></li>
+                        <li>The top 3 positions will be awarded with a Digital Certificate which will be sent on their reistered email-ids</li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+
+        <!---  FOOTER   --->
+
+        <div id="end">
+            <center>
+                <div class="footer-text">
+                    <a href="https://discord.gg/GCbVdag" class="page-links discord" target="_blank"><i class="fab fa-discord"></i></a>
+                    <a href="https://www.facebook.com/ordinatrix19.0/" class="page-links facebook" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                    <a href="mailto:ordinatrix10@gmail.com" class="page-links email" target="_blank"><i class="fas fa-envelope"></i></a>
+                    <div class="footer-copy font-alt">
+                        <a href="index">Home</a>
+                        |
+                        <a href="leaderboard">Leaderboard</a>
+                        |
+                        <a href="login">Login</a>
+                        |
+                        <a href="register">Register</a>
+                        |
+                        <a href="rules">rules</a>
+                    </div>
+                    <div class="footer-copy font-alt">
+                        © Pyrotech Club 2020
+                    </div>
+                </div>
+            </center>
+        </div>
+
+    </div>
+
+    <script src="index.js"></script>
+</body>
+
+</html>
