61da66d31da0d0b0887ca5d0e2278c2a3f68a9ce
Naman Chandok <namanchandok1@gmail.com>
2020-07-16 21:13:17 +0530

    sdfghjkl

--- crypt-release/shop.php
+++ crypt-release/shop.php
@@ -74,7 +74,7 @@ $htmlno = $result[0]??null;
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Dosis|Open+Sans" rel="stylesheet">
     <link href='main.css' rel='stylesheet' type='text/css'>
-    <link rel="icon" href="images/ordin.png" type="image/png">
+    <link rel="icon" href="images/favicon.png" type="image/png">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
